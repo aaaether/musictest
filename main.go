321f76722cd703d24dcda3639e4e979affb53757
aaaether <bekaaliyev848007@gmail.com>
2025-01-21 14:43:26 +0500

    Add basic backend

--- main.go
+++ main.go
@@ -1,21 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+type Request struct {
+	Name string `json:"name"`
+}
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	response := fmt.Sprintf("Hello, %s!", req.Name)
+	w.Write([]byte(response))
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/greet", greetHandler).Methods("POST")
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
-	}
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
